59270eeb62eeef281c312da9aa458d2b8176993c
tcoleman5 <47992464+tcoleman5@users.noreply.github.com>
2020-11-25 21:13:43 -0600

    Create products.java

---
+++ products.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class products {
+
+    private String products;
+    private double prices;
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String products) {
+        this.products = products;
+    }
+
+    public double getPrices() {
+        return prices;
+    }
+
+    public void setPrices(double prices) {
+        this.prices = prices;
+    }
+}
